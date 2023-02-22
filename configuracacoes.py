c395d74bd1aa32a0272ea91401ef6243d223058a
gabriel.laurindo.1 <gabriel.laurindo.1@outlook.com>
2023-02-22 20:46:38 -0300

    Adição constantes de configuração  de layout

--- configuracacoes.py
+++ configuracacoes.py
@@ -1,2 +1,8 @@
-NAME_SIZE = 15
+# configurações interface gráfica
+WINDOW_MAIN_SIZE = 500, 300
+MAX_SIZE_WIDTH = WINDOW_MAIN_SIZE[0]
+NAME_SIZE = 10 # Tamanho espaçamento dos labels
+LINE_SIZE_ELEMENT  = MAX_SIZE_WIDTH - NAME_SIZE
+
+# Configurações conversão de voz
 MAX_RATE = 200
